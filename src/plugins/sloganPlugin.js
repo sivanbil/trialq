b71ae64f3636a4447adea6e6ea7e4f44cafc7ba0
sivanliaobil <sivanliaobil@gmail.com>
2025-02-13 09:10:04 +0800

    version

--- src/plugins/sloganPlugin.js
+++ src/plugins/sloganPlugin.js
@@ -4,6 +4,7 @@ export default {
         const currentYear = new Date().getFullYear();
         app.config.globalProperties.$slogan = {
             main: 'TrialQ',
+            version: '0.0.1-beta',
             sub: '临研数据分析『超跑引擎』全速驱动突破，效率一路狂飙',
             year: currentYear, // 自动获取的当前年份
             email: 'trialqtool@gmail.com', // 固定的电子邮件地址
